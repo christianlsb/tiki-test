85e71ed06b528fed464ac9c1ae3894dd92187246
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-01-14 20:21:57 -0300

    feat: error message removed

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -11,32 +11,23 @@ export function Form() {
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
-    setIsSubmitting(true);
-
-    setTimeout(() => {
-      setIsSubmitting(false);
-    }, 3000);
+    if (formState.isValid) {
+      setIsSubmitting(true);
+      setTimeout(() => {
+        setIsSubmitting(false);
+      }, 3000);
+    }
   };
 
   const schema = yup
     .object()
     .shape({
-      name: yup
-        .string()
-        .required("O nome é obrigatório")
-        .min(4, "O nome precisa ter ao menos 4 caracteres"),
-      email: yup
-        .string()
-        .email("O email inserido não é valido")
-        .required("O email é obrigatório"),
+      name: yup.string().required().min(4),
+      email: yup.string().email().required(),
     })
     .required();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
+  const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -47,19 +38,17 @@ export function Form() {
       <S.ContainerForm>
         <S.TitleForm>CADASTRE-SE E FIQUE POR DENTRO DAS NOVIDADES</S.TitleForm>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <S.ErrorMenssage>{errors.name?.message}</S.ErrorMenssage>
           <Input
             type={"text"}
             placeholder={"Nome"}
             {...register("name")}
-            error={errors.name?.message}
+            error={formState.name?.message}
           />
-          <S.ErrorMenssage>{errors.email?.message}</S.ErrorMenssage>
           <Input
             type={"email"}
             placeholder={"meuemail@gmail.com"}
             {...register("email")}
-            error={errors.email?.message}
+            error={formState.email?.message}
           />
           <S.ContainerTermPrivacy>
             <InputRadio />
@@ -67,13 +56,9 @@ export function Form() {
               Declaro que li e aceito a política de privacidade
             </S.LabelForm>
           </S.ContainerTermPrivacy>
-          {isSubmitting ? (
-            <S.Button>Loading...</S.Button>
-          ) : (
-            <S.Button type="submit" handleSubmit={handleSubmitForm}>
-              Enviar
-            </S.Button>
-          )}
+          <S.Button type="submit" onClick={handleSubmitForm}>
+            {isSubmitting ? "Loading..." : "Enviar"}
+          </S.Button>
         </form>
       </S.ContainerForm>
     </>
