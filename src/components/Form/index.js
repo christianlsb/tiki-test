b5fd1d7fd43130741d70163c0fb3ec6451983f3b
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-01-14 17:27:24 -0300

    feat: functionality t send the completed form data

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,22 +1,37 @@
 import * as S from "./styles";
 import { Input } from "./Input/styles";
-import { Button } from "../Button";
-import {InputRadio} from "./InputRadio"
+import { InputRadio } from "./InputRadio";
+import { useState } from "react";
 
 export function Form() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setIsSubmitting(true);
+
+    setTimeout(() => {
+        setIsSubmitting(false);
+    }, 3000); 
+  };
+
   return (
     <>
       <S.ContainerForm>
         <S.TitleForm>CADASTRE-SE E FIQUE POR DENTRO DAS NOVIDADES</S.TitleForm>
-        <Input placeholder={"Nome"} />
-        <Input placeholder={"meuemail@gmail.com"} />
+        <Input type={"text"} placeholder={"Nome"} />
+        <Input type={"email"} placeholder={"meuemail@gmail.com"} />
         <S.ContainerTermPrivacy>
-          <InputRadio/>
+          <InputRadio />
           <S.LabelForm>
             Declaro que li e aceito a política de privacidade
           </S.LabelForm>
         </S.ContainerTermPrivacy>
-        <Button>Teste</Button>
+        {isSubmitting ? (
+          <S.Button>Loading...</S.Button>
+        ) : (
+          <S.Button type onClick={handleSubmit}>Teste</S.Button>
+        )}
       </S.ContainerForm>
     </>
   );
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -41,3 +41,24 @@ export const LabelForm = styled.label`
   font-weight: ${themes.fontWeight.normal};
   font-size: ${themes.fontSize.xs};
 `;
+
+export const Loading = styled.p`
+
+`
+
+
+export const Button = styled.button`
+    width: 425px;
+    height: 70px;
+    border-radius: 40px;
+    cursor: pointer;
+    box-shadow: 1px solid ${themes.colors.pink_500};
+    background-color: ${themes.colors.white};
+    font-size: ${themes.fontSize["2xl"]};
+    font-weight: ${themes.fontWeight.bold};
+    color: ${themes.colors.background};
+    border: none;
+    box-shadow: 3px 5px 0px ${themes.colors.pink_500};
+    background-color: ${themes.colors.white};
+    margin-top: 15px;
+`
